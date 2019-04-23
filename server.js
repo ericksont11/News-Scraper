93d2dca4bf3b85dd20f82e32def4eb5e8d69b990
ericksont11 <ericksont91@gmail.com>
2019-04-22 23:55:08 -0400

    changes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nprArticles";
 
-mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", (req, res) => {
