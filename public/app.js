04dda8dab1a9c72534e5d36d3e8a66901a05e45d
ericksont11 <ericksont91@gmail.com>
2019-04-22 23:42:51 -0400

    changes

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
-// Grab the articles as a json
+
 
 
 $.getJSON("/articles", data => {
-  // For each one
+
   for (let i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
+
     $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].teaser +"</p>");
   }
 });
