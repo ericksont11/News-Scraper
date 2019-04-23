0add29ba0daa3c7b2fde6386b21ea9860c6ae88c
ericksont11 <ericksont91@gmail.com>
2019-04-23 01:16:14 -0400

    styling changes

--- public/app.js
+++ public/app.js
@@ -1,12 +1,20 @@
 
+$(document).ready(function(){
+$("#btn").click( ()=> {
 
-
+console.log("yes")
 $.getJSON("/articles", data => {
 
   for (let i = 0; i < data.length; i++) {
 
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].teaser +"</p>");
+    $("#articles").append("<div class='col s12 m7'><h5 class='header'>"
+    +data[i].title+"</h5><div class='card horizontal'><div class='card-image'><img src="
+    +data[i].img+"></div><div class='card-stacked'><div class='card-content'><p>"
+    +data[i].teaser+"</p></div><div class='card-action'><a href="+data[i].link+">"+data[i].link+"</a></div></div></div></div>");
   }
 });
 
 
+})
+
+})
